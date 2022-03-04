4c2e0aeaaf203ffc43cf205b0d196ebfd6fa4d71
tingyuan <tingyuan@iflytek.com>
2022-03-04 19:54:44 +0800

    feat:num704 二分查找

---
+++ Algorithm/app/src/main/java/com/example/algorithm/base/num_704.java
@@ -0,0 +1,24 @@
+package com.example.algorithm.base;
+
+/**
+ * @description: 二分查找
+ * @author: tingyuan
+ * @date: 2022/3/4
+ */
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length -1;
+        while(left <= right) {
+            int mid = left + (right - left)/2;
+            if(nums[mid] > target) {
+                right = mid -1;
+            } else if(nums[mid]< target){
+                left = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
